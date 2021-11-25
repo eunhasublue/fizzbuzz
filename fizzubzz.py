18c6096b31419e9eca0070a8ba61eac26da2d37c
username <username@naver.com>
2021-11-25 15:19:17 +0900

    feat: Print 'buzz' if i is times of 5
    TODO:
    - print fizzbuzz if i is times of 15

--- fizzubzz.py
+++ fizzubzz.py
@@ -10,6 +10,8 @@ def do_fizzbuzz():
     for i in range(1,15+1):
         if i%3==0:
             print('fizz')
+        elif i%5==0:
+            print('buzz')
         else:
             print('{}'.format(i))
     return True
