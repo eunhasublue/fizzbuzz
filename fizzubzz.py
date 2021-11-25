12adaa25cb9e1f8e26a1b8175249b55298e89902
username <username@naver.com>
2021-11-25 15:21:30 +0900

    docs: Add done to show it's done

--- fizzubzz.py
+++ fizzubzz.py
@@ -2,10 +2,10 @@ def do_fizzbuzz():
     """
     Do fizzbuzz with for and if
     While i goes up 1 to 15,
-    print 'fizz' if i is times of 3
-    print 'buzz' if i is times of 5
+    print 'fizz' if i is times of 3(done)
+    print 'buzz' if i is times of 5(done)
     print 'fizzbuzz' if i is times of 15
-    else, print i (string)
+    else, print i (done, string)
     """
     for i in range(1,15+1):
         if i%3==0:
